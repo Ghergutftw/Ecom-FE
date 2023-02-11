80ca34a4522402b7deb7f6a90a0e23d9f68e31ef
Ghergut Madalin <madalinghergut@gmail.com>
2023-02-11 18:45:56 +0200

    Sectiunea 19 Done

--- angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import {CartService} from "../../services/cart.service";
+import {Subscription} from "rxjs";
+import {FormService} from "../../services/form.service";
 
 @Component({
   selector: 'app-checkout',
@@ -10,13 +13,18 @@ export class CheckoutComponent implements OnInit {
 
   checkoutFormGroup!: FormGroup;
 
-  totalPrice: number = 0;
+  totalPrice: number = 0.00;
   totalQuantity: number = 0;
 
-  constructor(private formBuilder: FormBuilder) { }
+  creditCardYears : number[] = [];
+  creditCardMonths : number[] = [];
+
+  constructor(private formBuilder: FormBuilder,
+              private cartService: CartService,
+              private formService: FormService) { }
 
   ngOnInit(): void {
-
+    this.getReview();
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
         firstName: [''],
@@ -46,6 +54,18 @@ export class CheckoutComponent implements OnInit {
         expirationYear: ['']
       })
     });
+    const startMonth : number = new Date().getMonth() + 1;
+    console.log("start month : " + startMonth)
+    this.formService.getCreditCardMonths(startMonth).subscribe(value => {
+      console.log("Credit card months : " + JSON.stringify(value));
+      this.creditCardMonths = value;
+    })
+    const startYear : number = new Date().getFullYear() + 1;
+    console.log("start year : " + startYear)
+    this.formService.getCreditCardYear().subscribe(value => {
+      console.log("Credit card year : " + JSON.stringify(value));
+      this.creditCardYears = value;
+    })
   }
 
   copyShippingAddressToBillingAddress({event}: { event: any }) {
@@ -55,11 +75,16 @@ export class CheckoutComponent implements OnInit {
           .setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
     }
     else {
-      this.checkoutFormGroup.controls["billingAddress"].reset();
+      this.checkoutFormGroup.controls['billingAddress'].reset();
     }
 
   }
 
   onSubmit() {
+  }
+
+  private getReview() {
+    this.cartService.totalPrice.subscribe(value => this.totalPrice = value);
+    this.cartService.totalQuantity.subscribe(value => this.totalQuantity = value);
   }
 }
